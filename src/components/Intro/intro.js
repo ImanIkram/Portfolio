79e07f6adcb253878c37661708635a30d2c9f74d
Iman Ikram <iman.ikram1122@gmail.com>
2024-02-27 17:27:29 -0500

    About WIP

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './intro.css';
-import bg from "../../assets/intro1.png"
+import bg from "../../assets/intro1.png";
+import { Link } from 'react-scroll';
+import { IoBag } from "react-icons/io5";
 
 const Intro = () => {
   return (
     <section id="intro">
         <div className="introContent">
             <span className='hello'>Hello,</span>
-            <span className='introText'>I'am Iman</span>
+            <span className='introText'>I'am <span className='introName'>Iman</span><br />Student</span>
+            <p className='introPara'> I am a Daedalus Schoalr Student at Hunter College pursing my Bachelors in Computer Science. Combining my passion for technology with my love for design and innovation, I thrive on crafting imaginative solutions to complex problems.</p>
+            <Link><button className='btn'><IoBag/>Hire Me</button></Link>
         </div>
         <img src={bg} alt="" className="bg"/>
 
