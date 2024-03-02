ab0196a0a705ce8611665b3480749a66fa45f7fe
Iman Ikram <iman.ikram1122@gmail.com>
2024-03-01 20:09:27 -0500

    WIP Intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
         <div className="introContent">
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Iman</span><br />Student</span>
-            <p className='introPara'> I am a Daedalus Schoalr Student at Hunter College pursing my Bachelors in Computer Science. Combining my passion for technology with my love for design and innovation, I thrive on crafting imaginative solutions to complex problems.</p>
+            <p className='introPara'> I am a Daedalus Scholar Student at Hunter College pursing my Bachelors in Computer Science. Combining my passion for technology with my love for design and innovation, I thrive on crafting imaginative solutions to complex problems.</p>
             <Link><button className='btn'><IoBag/>Hire Me</button></Link>
         </div>
         <img src={bg} alt="" className="bg"/>
