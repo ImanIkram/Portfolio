305d3841cf65bf1daad8d5ea4671c948a6c8072f
Iman Ikram <iman.ikram1122@gmail.com>
2024-08-01 12:27:46 -0400

    updating intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css';
-import bg from "../../assets/intro1.png";
+import bg from "../../assets/intro.png";
 import { Link } from 'react-scroll';
 import { IoBag } from "react-icons/io5";
 
@@ -10,7 +10,7 @@ const Intro = () => {
         <div className="introContent">
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Iman</span><br/>Student</span>
-            <p className='introPara'> I am a Daedalus Scholar Student at Hunter College pursing my Bachelors in Computer Science. Combining my passion for technology with my love for design and innovation, I thrive on crafting imaginative solutions to complex problems.</p>
+            <p className='introPara'> I am a Daedalus Honors Scholar at Hunter College pursing my Bachelors in Computer Science. Combining my passion for technology with my love for design and innovation, I thrive on crafting imaginative solutions to complex problems.</p>
             <Link><button className='btn'><IoBag/>  Hire Me</button></Link>
         </div>
         <img src={bg} alt="" className="bg"/>
